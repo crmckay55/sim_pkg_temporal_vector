e6ca77a2a5f239f08c18980debe371128a1605f0
cmckay <crmckay55@gmail.com>
2022-11-12 13:58:57 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='temporalvector',
+        version='0.0.0',
+        packages=['temporalvector'],
+        url='',
+        license='',
+        author='Chris McKay',
+        author_email='crmckay55@gmail.com',
+        description='Temporally aware vector of values'
+)
