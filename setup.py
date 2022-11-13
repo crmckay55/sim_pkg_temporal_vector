51820efd784baa5d608d254be83362cae345315b
cmckay <crmckay55@gmail.com>
2022-11-13 12:35:14 -0700

    Final Committ of package
    Took 1 hour 13 minutes

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup
 
 setup(
         name='temporalvector',
-        version='0.0.0',
+        version='1.0.0',
         packages=['temporalvector'],
-        url='',
-        license='',
+        url='https://git.jetbrains.space/cmckay/advanced-simulation/pkg_temporal_vector.git',
+        license='MIT',
+        install_requires=['numpy>=1.23.4'],
         author='Chris McKay',
         author_email='crmckay55@gmail.com',
-        description='Temporally aware vector of values'
+        description='Temporally aware vector of values that can be aggregated using different methods over different'
+                    'time periods'
 )
